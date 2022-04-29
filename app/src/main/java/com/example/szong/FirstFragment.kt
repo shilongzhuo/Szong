6dac3624559d8ada004ee58368b61cf20c8a2c21
shilongzhuo <1583161097@qq.com>
2022-04-29 16:44:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/szong/FirstFragment.kt
@@ -0,0 +1,39 @@
+package com.example.szong
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    fun doSomething(view: View?) { //方法必须定义为public
+//        val tv = findViewById<TextView>(R.id.textview_first)
+    }
+
+
+}
