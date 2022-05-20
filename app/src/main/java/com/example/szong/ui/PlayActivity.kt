65e5f6ca3260462eab13c050350c9b8a54e75506
shilongzhuo <1583161097@qq.com>
2022-05-20 14:36:12 +0800

    主界面设计

---
+++ app/src/main/java/com/example/szong/ui/PlayActivity.kt
@@ -0,0 +1,12 @@
+package com.example.szong.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.szong.R
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
