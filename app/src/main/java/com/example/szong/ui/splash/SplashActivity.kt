e2a5fe7654cd4f7077571448ff645e93f3fb9e1e
shilongzhuo <1583161097@qq.com>
2022-06-07 19:40:57 +0800

    fit 初始动画

--- app/src/main/java/com/example/szong/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/szong/ui/splash/SplashActivity.kt
@@ -1,27 +1,33 @@
 package com.example.szong.ui.splash
 
 import android.content.Intent
-import com.example.szong.databinding.ActivitySplashBinding
-import com.example.szong.ui.base.BaseActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import com.airbnb.lottie.LottieAnimationView
+import com.example.szong.R
 import com.example.szong.ui.main.MainActivity
 
-/**
- * 启动页 Activity
- */
-class SplashActivity : BaseActivity() {
+class SplashActivity : AppCompatActivity() {
+    private lateinit var appname: TextView
+    private lateinit var lottie:LottieAnimationView
+    private lateinit var appimage:LottieAnimationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
-    private lateinit var binding: ActivitySplashBinding
+        appname = findViewById(R.id.appname)
+        lottie = findViewById(R.id.lottie)
+//        appimage = findViewById(R.id.appimage)
+        appname.animate().translationY(-600f).setDuration(2700).setStartDelay(0)
+//        lottie.animate().translationX(2000f).setDuration(2000).setStartDelay(0)
+        Handler().postDelayed(object : Runnable {
+            override fun run(){
+                val intent = Intent(applicationContext,MainActivity::class.java)
+                startActivity(intent)
+            }
+        },3000)
 
-    override fun initBinding() {
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(binding.root)
     }
-
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
 }
